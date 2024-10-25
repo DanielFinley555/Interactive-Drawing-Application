da512b6439fb82e4b4da4b3fbf31b4bb756de4a4
John Doe <johndoe@email.com>
2024-10-25 00:19:53 -0400

    Setup HTML structure for the drawing application.

---
+++ app.js
@@ -0,0 +1,9 @@
+//Task 1: Set Up the HTML Structure
+//Done in HTML file
+
+
+//Task 2: Configure the JavaScript for Drawing Context
+const canvas document.getElementById("myCanvas")
+const ctx = canvas.getContext("2d")
+
+ctx
