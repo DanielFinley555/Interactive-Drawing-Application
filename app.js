2a6fbd8e8411bc539579c09833cc4800f7a591c7
John Doe <johndoe@email.com>
2024-10-25 20:18:43 -0400

    Implement shape drawing logic for lines, rectangles, and circles.

--- app.js
+++ app.js
@@ -17,15 +17,41 @@ let opacity = 1;
 
 //Task 3: Implement Shape Drawing Logic
 
-
+//creating a requirement for the user to have to click on lines to allow for drawing of lines
+//using querySelector to bypass having to create the variable again in the js file
+document.querySelector("#lines").addEventListener("click", () =>{
 
 //function for drawing line
+function drawLine(x, y) {
+    ctx.strokeStyle = `rgba(0,0, 255, ${opacity})`;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(0, 0);
+    ctx.stroke();
+}
+canvas.addEventListener("click", e => {
+    drawLine(e.offsetX, e.offsetY)
+});
+
+})
+
+//creating a requirement to click on rectangles
+document.querySelector("#rectangles").addEventListener("click", () =>{
 
 //function for drawing rectangles
+function drawRectangle(x, y) {
+    ctx.strokeStyle = `rgba(0,0, 255, ${opacity})`;
+    ctx.beginPath();
+    ctx.rect(x,y, 50,30);
+    ctx.stroke();
+}
+canvas.addEventListener("click", e => {
+    drawRectangle(e.offsetX, e.offsetY)
+});
 
+})
 
-//creating a requirement for the user to have to click on circles to allow for drawing of circles
-//using querySelector to bypass having to create a variable
+//creating a requirement to click on circles
 document.querySelector("#circles").addEventListener("click", () =>{
     
 //function for drawing circles
